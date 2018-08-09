3a9dd9cbf9337afb4936f37315585efde2e65e54
Hackbright Student <no-reply@hackbrightacademy.com>
2018-08-08 21:01:44 -0700

    finished student added form

--- hackbright_web.py
+++ hackbright_web.py
@@ -27,10 +27,23 @@ def get_student_form():
 
     return render_template("student_search.html")
 
-@app.route("/student-add", methods=['POST'])
+@app.route("/student-add")
 def student_add():
-	"""Add a student."""
+	"""Add a student Form."""
 
+	return render_template("student_add.html")
+
+@app.route("/student-added", methods=['POST'])
+def student_added():
+	"""Add a student Confirmation."""
+	first_name = request.form.get("first_name")
+	last_name = request.form.get("last_name")
+	github = request.form.get("github")
+
+	hackbright.make_new_student(first_name, last_name, github)
+
+
+	return render_template ("student_added.html")
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
